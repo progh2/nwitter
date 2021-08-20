bdccf880d737f0d52b89d9243594e2b05fae088c
Gihun Ham <ham@e-mirim.hs.kr>
2021-08-20 16:07:25 +0900

    firebase key 분리

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase/app";
 
 const firebaseConfig = {
-apiKey: "AIzaSyBRWlP-vNlLbiWySjRnJMmKENZTStu7Loo",
-authDomain: "nwitter-b97aa.firebaseapp.com",
-projectId: "nwitter-b97aa",
-storageBucket: "nwitter-b97aa.appspot.com",
-messagingSenderId: "115973244981",
-appId: "1:115973244981:web:3d05b1c692bac724fa897c"
+apiKey: process.env.REACT_APP_API_KEY,
+authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+projectId: process.env.REACT_APP_PROJECT_ID,
+storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+messagingSenderId: process.env.REACT_APP_MESSAGIN_SENDER_ID,
+appId: process.env.REACT_APP_APP_ID
 };
 // Initialize Firebase
 //firebase.initializeApp(firebaseConfig);
